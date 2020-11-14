e56558f65a0ad01995929dbe5e45a50955984ec6
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2020-11-14 15:31:39 +0400

    Password validation added

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,5 @@
 class Registered{
-    constructor(firstName, lastName, birthday, gender, email, phone, subject) {
+    constructor(firstName, lastName, birthday, gender, email, phone, password) {
 
         this.firstName = firstName;
         this.lastName = lastName;
@@ -7,11 +7,21 @@ class Registered{
         this.gender = gender;
         this.email = email;
         this.phone = phone;
-        this.subject = subject;
+        this.password = password;
     }
 
     get info() {
         return "First Name: " + this.firstName + "\nLast Name: " + this.lastName + "\nBirthday: " + this.birthday + "\nGender: " + this.gender + "\nEmail: " + this.email + "\nPhone: " + this.phone + "\nSubject: " + this.subject;
+    }
+
+    save(){
+        localStorage.setItem('firstName', this.firstName);
+        localStorage.setItem('lastName', this.lastName);
+        localStorage.setItem('birthday', this.birthday);
+        localStorage.setItem('gender', this.gender);
+        localStorage.setItem('email', this.email);
+        localStorage.setItem('phone', this.phone);
+        localStorage.setItem('password', this.password);
     }
 };
 
@@ -23,24 +33,26 @@ var birthday = document.querySelector('#birthday');
 var gender = document.querySelectorAll('#gender');
 var email = document.querySelector('#email');
 var phone = document.querySelector('#phone');
-var subject = document.querySelector('#subject');
+var password = document.querySelector('#password');
+var password_confirmation = document.querySelector('#passwordConf');
 var submit = document.querySelector('#submit');
 
 var inputs = document.querySelectorAll('input');
 
 submit.addEventListener('click', function (e) {
     var x = validate_on_submit();
-    var gender_name = "Male";
-    if (gender[1].checked) {
-        gender_name = "Female";
-    }
-    var user = new Registered(firstName.value, lastName.value, birthday.value, gender_name, email.value, phone.value, subject.value);
     if (x == false) {
         alert('Please full the form proparly!');
         e.preventDefault();
         return false;
     }
+    var gender_name = "Male";
+    if (gender[1].checked) {
+        gender_name = "Female";
+    }
+    var user = new Registered(firstName.value, lastName.value, birthday.value, gender_name, email.value, phone.value, subject.value);
     alert("User Registered Succesfully!\n" + user.info);
+    user.save();
 });
 
 function validate_on_submit() {
@@ -55,17 +67,14 @@ function validate_on_submit() {
                 feedback.innerHTML = "";
             }
         }
-        feedback = subject.parentNode.lastElementChild;
-        if (subject.value == "-1") {
-            feedback.innerHTML = "This field can't be empty!";
-            check = false;
-        } else {
-            feedback.innerHTML = "";
-        }
     }
     var email_valid = email_check();
     if (check == true) {
         check = email_valid;
+    }
+    var password_valid = password_check();
+    if (check == true) {
+        check = password_valid;
     }
     return check;
 }
@@ -80,4 +89,65 @@ function email_check() {
         return false;
     }
     return true;
+}
+
+function password_check(){
+    if (password.value.length<8) {
+        var feedback = password.parentNode.lastElementChild;
+        if (feedback.innerHTML.length == 0) {
+            feedback.innerHTML = "Password must contain at least 8 character!";
+        }
+        password_reset()
+        return false;
+    }
+    var lower = new RegExp("^(?=.*[a-z])");
+    if (!lower.test(password.value)) {
+        var feedback = password.parentNode.lastElementChild;
+        if (feedback.innerHTML.length == 0) {
+            feedback.innerHTML = "Password must contain at least 1 lowercase alphabetical character";
+        }
+        password_reset()
+        return false;
+    }
+    var upper = new RegExp("^(?=.*[A-Z])");
+    if (!upper.test(password.value)) {
+        var feedback = password.parentNode.lastElementChild;
+        if (feedback.innerHTML.length == 0) {
+            feedback.innerHTML = "Password must contain at least 1 uppercase alphabetical character";
+        }
+        password_reset()
+        return false;
+    }
+    var num = new RegExp("^(?=.*[0-9])");
+    if (!num.test(password.value)) {
+        var feedback = password.parentNode.lastElementChild;
+        if (feedback.innerHTML.length == 0) {
+            feedback.innerHTML = "Password must contain at least 1 numeric character";
+        }
+        password_reset()
+        return false;
+    }
+    var spec = new RegExp("^(?=.*[!@#$%^&*])");
+    if (!spec.test(password.value)) {
+        var feedback = password.parentNode.lastElementChild;
+        if (feedback.innerHTML.length == 0) {
+            feedback.innerHTML = "Password must contain at least one special character! (!@#$%^&*)";
+        }
+        password_reset()
+        return false;
+    }
+    if(password.value!=password_confirmation.value){
+        var feedback = password_confirmation.parentNode.lastElementChild;
+        if (feedback.innerHTML.length == 0) {
+            feedback.innerHTML = "This field must be same with the password field!";
+        }
+        password_reset()
+        return false;
+    }
+    return true;
+}
+
+function password_reset(){
+    password.value = "";
+    password_confirmation.value = "";
 }
